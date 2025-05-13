c8f1d8dff10d4fef2cdb40e71a0d82a9691da305
Negar Babaei <negarbabaei377@gmail.com>
2025-05-13 21:44:16 +0330

    feat: Http configuration

---
+++ src/service/http.service.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import {TOKEN} from "../config/variables.config";
+import {PATH} from '../config/path.config';
+import {LOGIN} from "../config/endpoint.config";
+import {toast} from 'react-toastify';
+
+class Http {
+    constructor() {
+        axios.defaults.baseURL = "https://mock.arianalabs.io/api"
+        axios.interceptors.request.use((config) => {
+            const token = localStorage.getItem(TOKEN)
+            if (token !== undefined && token !== "" && config.url !== LOGIN) {
+                config.headers['token'] = `${token}`
+            }
+            return config
+        }, (error) => {
+            return Promise.reject(error.response.data)
+        })
+        axios.interceptors.response.use((response) => {
+            return response
+        }, (error) => {
+            if (error.response.status === 401) {
+                localStorage.removeItem(TOKEN)
+                window.location.href = PATH.LOGIN
+            } else {
+                toast.error(error.response.data)
+            }
+        })
+    }
+
+    get(url, config) {
+        return axios.get(url, config)
+    }
+
+    post(url, data, config) {
+        return axios.post(url, data, config)
+    }
+
+    delete(url, config) {
+        return axios.delete(url, config)
+    }
+}
+
+export default new Http;
